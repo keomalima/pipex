22b80cce07a28b0b094e656c45a41c7c4c831c37
Keoma Lima <keomalima@outlook.com>
2024-12-20 13:24:30 +0100

    file access check implemented

--- pipex.c
+++ pipex.c
@@ -3,26 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kricci-d <kricci-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: keomalima <keomalima@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 11:15:08 by kricci-d          #+#    #+#             */
-/*   Updated: 2024/12/19 11:33:53 by kricci-d         ###   ########.fr       */
+/*   Updated: 2024/12/20 13:23:44 by keomalima        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pipex(void)
+int	access_check(char **av, t_filed *file)
 {
-	ft_printf("Line len %i\n", ft_strlen("Keoma"));
+	int	access_write;
+
+	access_write = access(av[4], W_OK);
+	file->fd = open(av[1], O_RDONLY);
+	if (!file->fd || access_write == -1)
+	{
+		if (!file->fd)
+			ft_printf("%s: %s\n"RESET, strerror(errno), av[1]);
+		else
+			ft_printf("%s: %s\n"RESET, strerror(errno), av[4]);
+		if (file->fd != -1)
+			close(file->fd);
+		return (1);
+	}
+	file->infile = av[1];
+	file->first_cmd = av[2];
+	file->second_cmd = av[3];
+	file->outfile = av[4];
+	return (0);
+}
+
+int	pipex(char **av)
+{
+	t_filed	file_info;
+
+	if (access_check(av, &file_info) == 1)
+		return (1);
+	return (0);
 }
 
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
-
-	pipex();
+	if (ac == 5)
+	{
+		if (pipex(av) == 1)
+			return (1);
+	}
+	else
+	{
+		if (ac < 5)
+			ft_printf(RED"Error: Too few arguments\n"RESET);
+		else
+			ft_printf(RED"Error: Too many arguments\n"RESET);
+		return (1);
+	}
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kricci-d <kricci-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: keomalima <keomalima@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 11:17:07 by kricci-d          #+#    #+#             */
-/*   Updated: 2024/12/19 11:19:09 by kricci-d         ###   ########.fr       */
+/*   Updated: 2024/12/20 13:12:47 by keomalima        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define PIPEX_H
 
 # include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 # include <stdio.h>
 # include <limits.h>
 # include <fcntl.h>
@@ -21,5 +23,20 @@
 # include "libft/libft.h"
 # include "libft/ft_printf.h"
 # include "libft/get_next_line.h"
+
+#define RESET "\x1b[0m"   // Reset color
+#define RED "\x1b[31m"  // Red
+#define GREEN "\x1b[32m"  // Green
+#define YELLOW "\x1b[33m"  // Yellow
+#define BLUE "\x1b[34m"  // Blue
+
+typedef struct s_filed
+{
+	int		fd;
+	char	*infile;
+	char	*outfile;
+	char	*first_cmd;
+	char	*second_cmd;
+}					t_filed;
 
 #endif
